2903a67faa310d653ba82aef5203bb51b1b6b3f8
Mark Venz <mrkvnz+github@gmail.com>
2016-12-11 16:51:09 +1000

    Inital basic comments

--- robot.ino
+++ robot.ino
@@ -2,15 +2,21 @@
  * Programming a robot (mBot)
  */
 
+
 /* ********************************************************************
  * start up
+ *
+ * anything run once at start-up / reboot should be added here
  */
 void setup () {
 
 }
 
+
 /* ********************************************************************
  * loop
+ *
+ * run forever
  */
 void loop () {
 
